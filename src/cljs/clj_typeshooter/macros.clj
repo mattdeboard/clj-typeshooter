0b07918de6d6752f84de3dc84e306cbeef8dde3e
Matt DeBoard <matt.deboard@gmail.com>
2013-09-28 15:33:59 -0400

    Macro comments.

--- src/cljs/clj_typeshooter/macros.clj
+++ src/cljs/clj_typeshooter/macros.clj
@@ -1,7 +1,9 @@
 (ns clj-typeshooter.macros)
 
 (defmacro go-handle! [update-ch & body]
-  "Execute `body' inside goroutine, then send data to channel `update-ch'."
+  "Execute `body' inside goroutine, then send data to channel `update-ch'.
+This basically is callback functionality without the spaghetti. The
+'callback function' is a consumer of `update-ch'."
   `(cljs.core.async.macros/go
     (do ~@body)
     (cljs.core.async/>! ~update-ch 1)))
